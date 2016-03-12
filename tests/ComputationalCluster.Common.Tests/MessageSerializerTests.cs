f6e738f633f32643af606391d93becab7595e045
Marcin Chudy <chudym@student.mini.pw.edu.pl>
2016-03-12 19:02:00 +0100

    Add unit tests for deserializing messages

--- tests/ComputationalCluster.Common.Tests/MessageSerializerTests.cs
+++ tests/ComputationalCluster.Common.Tests/MessageSerializerTests.cs
@@ -1,6 +1,52 @@
-﻿namespace ComputationalCluster.Common.Tests
+﻿using ComputationalCluster.Common.Messages;
+using ComputationalCluster.Common.Serialization;
+using System.IO;
+using Xunit;
+
+namespace ComputationalCluster.Common.Tests
 {
     public class MessageSerializerTests
     {
+        [Fact]
+        public void Deserialize_ShouldReturnCorrectType()
+        {
+            var xml = File.ReadAllText("SampleMessages/Register.xml");
+            var serializer = new MessageSerializer();
+
+            var message = serializer.Deserialize(xml);
+
+            Assert.Equal(typeof(RegisterMessage), message.GetType());
+        }
+
+        [Fact]
+        public void Deserialize_ShouldAssignProperties()
+        {
+            var message = DeserializeSampleRegisterMessage();
+
+            Assert.Equal(12u, message.Id);
+            Assert.Equal(8, message.ParallelThreads);
+            Assert.Equal(false, message.Deregister);
+            Assert.Equal(RegisterType.TaskManager, message.Type);
+        }
+
+        [Fact]
+        public void Deserialize_ShouldAssignSolvableProblems()
+        {
+            var message = DeserializeSampleRegisterMessage();
+
+            Assert.Equal(2, message.SolvableProblems.Length);
+            Assert.Equal("TSP", message.SolvableProblems[0]);
+            Assert.Equal("DVRP", message.SolvableProblems[1]);
+        }
+
+        private static RegisterMessage DeserializeSampleRegisterMessage()
+        {
+            var xml = File.ReadAllText("SampleMessages/Register.xml");
+            var serializer = new MessageSerializer();
+
+            var message = (RegisterMessage)serializer.Deserialize(xml);
+            return message;
+        }
+
     }
 }
