d7a1555338222fa489270831df621fa1d7593f2f
Michał Berent <zaguada13@gmail.com>
2016-03-16 21:37:42 +0100

    Client sending message

---
+++ src/ComputationalCluster.Client/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComputationalCluster.Common;
+using ComputationalCluster.Common.Messages;
+using ComputationalCluster.Common.Messaging;
+
+namespace ComputationalCluster.Client
+{
+    class Client
+    {
+        private readonly IMessenger messenger;
+        private readonly IConfiguration configuration;
+        private ulong id;
+
+        public Client(IMessenger messenger, IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.messenger = messenger;
+        }
+
+        public void Start()
+        {
+            var message = new SolveRequestMessage()
+            {
+                ProblemType = "DVRP",
+                SolvingTimeout = 99999,
+                SolvingTimeoutSpecified = true,
+                Id = 123,
+                IdSpecified = false,
+                Data = new byte[] {1, 2, 3}              
+            };
+            try
+            {
+                IList<Message> responses = messenger.SendMessage(message);
+                var responseMessage = responses[0] as SolveRequestResponseMessage;
+                if (responseMessage != null)
+                {
+                    var response = responseMessage;
+                    id = response.Id;
+                    Console.WriteLine($"SolveRequestResponse with id {id}");
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- src/ComputationalCluster.Client/Program.cs
+++ src/ComputationalCluster.Client/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace ComputationalCluster.Client
+﻿using Autofac;
+using Autofac.Core;
+using ComputationalCluster.Common;
+using System;
+namespace ComputationalCluster.Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
 
+            builder.RegisterModule<CommonModule>();
+            builder.RegisterType<Client>()
+                .AsSelf()
+                .SingleInstance();
+
+            var container = builder.Build();
+
+            try
+            {
+                var client = container.Resolve<Client>();
+                client.Start();
+            }
+            catch (DependencyResolutionException e)
+            {
+                Console.Error.WriteLine(e.InnerException.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
