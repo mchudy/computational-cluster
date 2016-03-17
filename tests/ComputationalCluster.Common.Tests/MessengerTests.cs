77841a1b0a08cfc0e6dc580426119272acd66223
Marcin Chudy <chudym@student.mini.pw.edu.pl>
2016-03-17 16:42:02 +0100

    Add some tests for serializer

--- tests/ComputationalCluster.Common.Tests/MessengerTests.cs
+++ tests/ComputationalCluster.Common.Tests/MessengerTests.cs
@@ -4,6 +4,7 @@ using ComputationalCluster.Common.Networking;
 using ComputationalCluster.Common.Serialization;
 using Moq;
 using System.IO;
+using System.Text;
 using Xunit;
 
 namespace ComputationalCluster.Common.Tests
@@ -28,6 +29,29 @@ namespace ComputationalCluster.Common.Tests
             messenger.SendMessage(new RegisterMessage());
 
             tcpConnectionMock.Verify(t => t.Connect("aaa", 1000), Times.Once());
+        }
+
+        [Fact]
+        public void SendMessage_ShouldEndMessageWithETB()
+        {
+            var memoryStream = new MemoryStream();
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(c => c.ServerAddress).Returns("aaa");
+            configuration.Setup(c => c.ServerPort).Returns(1000);
+
+            var serializerMock = new Mock<IMessageSerializer>();
+            var tcpConnectionFactoryMock = new Mock<ITcpConnectionFactory>();
+            var tcpConnectionMock = new Mock<ITcpConnection>();
+            tcpConnectionMock.Setup(t => t.GetStream()).Returns(memoryStream);
+            tcpConnectionFactoryMock.Setup(f => f.Create()).Returns(tcpConnectionMock.Object);
+
+            var messenger = new Messenger(serializerMock.Object, configuration.Object, tcpConnectionFactoryMock.Object);
+            messenger.SendMessage(new RegisterMessage());
+
+            byte[] buffer = new byte[1000];
+            //memoryStream.Read(buffer, 0, 1000);
+            string test = Encoding.UTF8.GetString(buffer);
+            //Assert.Equal(Constants.ETB, test.Last());
         }
     }
 }
