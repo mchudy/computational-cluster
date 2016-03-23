c6adf08e0e2131eaef2bfb94316aa47ab167c6c6
Marcin Chudy <chudym@student.mini.pw.edu.pl>
2016-03-23 23:42:07 +0100

    Another fix

---
+++ src/ComputationalCluster.Server/BackupHandlers/NoOperationMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using ComputationalCluster.Common.Messages;
+using ComputationalCluster.Common.Messaging;
+
+namespace ComputationalCluster.Server.BackupHandlers
+{
+    public class NoOperationMessageHandler : IResponseHandler<NoOperationMessage>
+    {
+        private readonly IServerContext context;
+
+        public NoOperationMessageHandler(IServerContext context)
+        {
+            this.context = context;
+        }
+
+        public void HandleResponse(NoOperationMessage message)
+        {
+            context.BackupServers = message.BackupCommunicationServers;
+        }
+    }
+}
---
+++ src/ComputationalCluster.Server/BackupHandlers/RegisterResponseMessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using ComputationalCluster.Common.Messages;
+using ComputationalCluster.Common.Messaging;
+using log4net;
+
+namespace ComputationalCluster.Server.BackupHandlers
+{
+    public class RegisterResponseMessageHandler : IResponseHandler<RegisterResponseMessage>
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(RegisterResponseMessageHandler));
+        private readonly IServerContext context;
+
+        public RegisterResponseMessageHandler(IServerContext context)
+        {
+            this.context = context;
+        }
+
+        public void HandleResponse(RegisterResponseMessage message)
+        {
+            logger.Info($"Registered with id {message.Id}");
+            context.Id = (int)message.Id;
+            context.Configuration.Timeout = message.Timeout;
+        }
+    }
+}
