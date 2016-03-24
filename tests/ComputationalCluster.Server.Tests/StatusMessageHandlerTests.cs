806bdf3d3867c902814eed3ac92a6a9255094e9f
Mateusz Jabłoński <matjablonski94@gmail.com>
2016-03-24 15:12:59 +0100

    Adding Server Tests

--- tests/ComputationalCluster.Server.Tests/StatusMessageHandlerTests.cs
+++ tests/ComputationalCluster.Server.Tests/StatusMessageHandlerTests.cs
@@ -1,6 +1,17 @@
-﻿namespace ComputationalCluster.Server.Tests
+﻿using ComputationalCluster.Common.Messages;
+using ComputationalCluster.Common.Networking;
+using ComputationalCluster.Common.Objects;
+using ComputationalCluster.Server.Handlers;
+using Moq;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ComputationalCluster.Server.Tests
 {
     public class StatusMessageHandlerTests
     {
+       
     }
 }
