719e6add18bc8805ae58a838d67eb64384f75c31
Marcin Chudy <chudym@student.mini.pw.edu.pl>
2016-04-07 17:41:19 +0200

    Add empty TaskSolver for DVRP

---
+++ src/ComputationalCluster.Common/TaskSolverFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCCTaskSolver;
+
+namespace ComputationalCluster.Common
+{
+    public class TaskSolverFactory : TaskSolverCreator
+    {
+        public override TaskSolver CreateTaskSolverInstance(byte[] problemData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ComputationalCluster.DVRPTaskSolver/DVRPTaskSolver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ComputationalCluster.DVRPTaskSolver
+{
+    public class DVRPTaskSolver : UCCTaskSolver.TaskSolver
+    {
+        public override string Name => "DVRP";
+
+        public DVRPTaskSolver(byte[] problemData)
+            : base(problemData)
+        {
+        }
+
+        public override byte[][] DivideProblem(int threadCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] MergeSolution(byte[][] solutions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] Solve(byte[] partialData, TimeSpan timeout)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
