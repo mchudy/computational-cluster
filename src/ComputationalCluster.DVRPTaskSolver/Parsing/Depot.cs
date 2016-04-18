e7ea5d8a1b6b03dd3602859a5c426f54d8136db7
Mateusz Jabłoński <matjablonski94@gmail.com>
2016-04-19 01:54:19 +0200

    adding classes for DVRP problem

---
+++ src/ComputationalCluster.DVRPTaskSolver/Parsing/Depot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputationalCluster.DVRPTaskSolver.Parsing
+{
+    public class Depot
+    {
+        public Location Location { get; set; }
+        public int StartHour { get; set; }
+        public int EndHour { get; set; }
+    }
+}
