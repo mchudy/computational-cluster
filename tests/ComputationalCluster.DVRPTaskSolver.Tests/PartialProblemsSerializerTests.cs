0242299a2a6e25da6814726a0657aae7004e4826
Marcin Chudy <chudym@student.mini.pw.edu.pl>
2016-04-21 12:16:15 +0200

    Add tests for serializers

--- tests/ComputationalCluster.DVRPTaskSolver.Tests/PartialProblemsSerializerTests.cs
+++ tests/ComputationalCluster.DVRPTaskSolver.Tests/PartialProblemsSerializerTests.cs
@@ -22,5 +22,66 @@ namespace ComputationalCluster.DVRPTaskSolver.Tests
             });
             var json = Encoding.UTF8.GetString(data[0]);
         }
+
+        private string json = @"{
+                ""ProblemInstance"": {
+                    ""Clients"" : [
+                        {
+                            ""AvailableTime"" : 10,
+                            ""UnloadTime"" : 20,
+                            ""X"" : 4,
+                            ""Y"": 3,
+                        }
+                    ],
+                    ""Depots"" : [
+                        {
+                            ""X"" : 0,
+                            ""Y"": 1 ,
+                            ""StartTime"" : 30,
+                            ""EndTime"" : 50
+                        }
+                    ]
+                },
+                ""Partitions"": [
+                    {
+                        ""truckClients"": [
+                            [1,2],
+                            [3]
+                        ]
+                    }
+                ]
+            }";
+
+        [Fact]
+        public void ShouldDeserializePartialInstance()
+        {
+            var serializer = new PartialProblemsSerializer();
+            var problem = serializer.Deserialize(Encoding.UTF8.GetBytes(json));
+
+            var client = problem.ProblemInstance.Clients[0];
+            Assert.Equal(10, client.AvailableTime);
+            Assert.Equal(20, client.UnloadTime);
+            Assert.Equal(4, client.X);
+            Assert.Equal(3, client.Y);
+
+            var depot = problem.ProblemInstance.Depots[0];
+            Assert.Equal(0, depot.X);
+            Assert.Equal(1, depot.Y);
+            Assert.Equal(30, depot.StartTime);
+            Assert.Equal(50, depot.EndTime);
+        }
+
+        [Fact]
+        public void ShouldDeserializePartitions()
+        {
+            var serializer = new PartialProblemsSerializer();
+            var problem = serializer.Deserialize(Encoding.UTF8.GetBytes(json));
+
+            Assert.Equal(2, problem.Partitions[0].truckClients.Length);
+
+            Assert.Equal(1, problem.Partitions[0].truckClients[0][0]);
+            Assert.Equal(2, problem.Partitions[0].truckClients[0][1]);
+            Assert.Equal(3, problem.Partitions[0].truckClients[1][0]);
+        }
     }
 }
